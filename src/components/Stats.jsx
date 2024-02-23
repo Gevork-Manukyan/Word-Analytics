4b9e9baf9fe1bd82db40291d2ebe81a55596ebea
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-02-22 22:55:27 -0800

    finished stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,3 +1,24 @@
-export default function Stats() {
-  return <section className="stats">Stats</section>;
+export default function Stats({ text }) {
+  const numberOfCharacters = text.length;
+  const instagramCharactersLeft = numberOfCharacters > 280 ? 0 : 280 - numberOfCharacters;
+  const facebookCharactersLeft = numberOfCharacters > 2200 ? 0 : 2200 - numberOfCharacters;
+  const numberOfWords = text.split(' ').filter((word) => word !== "").length;
+
+  return (
+    <section className="stats">
+      <Stat number={numberOfWords} heading="Words" />
+      <Stat number={numberOfCharacters} heading="Characters" />
+      <Stat number={instagramCharactersLeft} heading="Instagram" />
+      <Stat number={facebookCharactersLeft} heading="Facebook" />
+    </section>
+  );
+}
+
+function Stat({ number, heading }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number}</span>
+      <h2 className="second-heading">{heading}</h2>
+    </section>
+  );
 }
--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,3 +1,37 @@
-export default function Textarea() {
-  return <textarea className="textarea" />;
+import { useState } from "react";
+import Warning from "./Warning";
+
+export default function Textarea({ text, setText }) {
+  const [warningText, setWarningText] = useState("");
+
+  const handleChange = (event) => {
+    let newText = event.target.value;
+
+    // basic validation to prevent XSS attacks
+    if (newText.includes("<script>")) {
+      setWarningText('No script tag allowed!');
+      newText = newText.replace("<script>", "");
+    } 
+    else if (newText.includes("@")) {
+      setWarningText('No @ sign allowed!');
+      newText = newText.replace("@", "");
+    }
+    else {
+      setWarningText("");
+    }
+
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        placeholder="Enter your message here..."
+        spellCheck="false"
+        onChange={handleChange}
+        value={text}
+      />
+      <Warning warningText={warningText} />
+    </div>
+  );
 }
