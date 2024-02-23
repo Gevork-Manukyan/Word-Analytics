933964c7324f59070c1a0fcd2e44d26794b8f379
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2024-02-22 23:04:36 -0800

    adds red error to stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,23 +1,25 @@
 export default function Stats({ text }) {
   const numberOfCharacters = text.length;
   const instagramCharactersLeft = numberOfCharacters > 280 ? 0 : 280 - numberOfCharacters;
+  const instagramLimit = instagramCharactersLeft === 0;
   const facebookCharactersLeft = numberOfCharacters > 2200 ? 0 : 2200 - numberOfCharacters;
+  const facebookLimit = facebookCharactersLeft === 0;
   const numberOfWords = text.split(' ').filter((word) => word !== "").length;
 
   return (
     <section className="stats">
       <Stat number={numberOfWords} heading="Words" />
       <Stat number={numberOfCharacters} heading="Characters" />
-      <Stat number={instagramCharactersLeft} heading="Instagram" />
-      <Stat number={facebookCharactersLeft} heading="Facebook" />
+      <Stat number={instagramCharactersLeft} heading="Instagram" limitReached={instagramLimit} />
+      <Stat number={facebookCharactersLeft} heading="Facebook" limitReached={facebookLimit} />
     </section>
   );
 }
 
-function Stat({ number, heading }) {
+function Stat({ number, heading, limitReached }) {
   return (
     <section className="stat">
-      <span className="stat__number">{number}</span>
+      <span className={`stat__number ${limitReached ? 'stat__number--limit' : ''}`}>{number}</span>
       <h2 className="second-heading">{heading}</h2>
     </section>
   );
